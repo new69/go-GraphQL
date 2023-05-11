3faffb51f398934a979f78c6d2255911541cc587
Jefferson de Oliveira Silva <spalla69@gmail.com>
2023-05-11 15:48:48 -0300

    First commit

---
+++ graph/resolver.go
@@ -0,0 +1,12 @@
+package graph
+
+import "github.com/new69/go-GraphQL/internal/database"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	CategoryDB *database.Category
+	CourseDB   *database.Course
+}
